4f6e38c06d71957c01f9580ba1d820fc852cdbea
wendyrs <wendy.reyes@wunderman.com>
2021-03-03 16:38:46 -0500

    gh-pages

---
+++ precache-manifest.432edebad8652167d5392bd9f3820b4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2de70b53ecfc536efe2",
+    "url": "/vue-pwa-calculator/css/app.c67cd003.css"
+  },
+  {
+    "revision": "575cccff1f677ea32f3e45009cbd8688",
+    "url": "/vue-pwa-calculator/index.html"
+  },
+  {
+    "revision": "d2de70b53ecfc536efe2",
+    "url": "/vue-pwa-calculator/js/app.39390d21.js"
+  },
+  {
+    "revision": "d838d13ef01d5fd04c9c",
+    "url": "/vue-pwa-calculator/js/chunk-vendors.190910eb.js"
+  },
+  {
+    "revision": "873f9278615237dd65e82b3898f2a316",
+    "url": "/vue-pwa-calculator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pwa-calculator/robots.txt"
+  }
+]);
